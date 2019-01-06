7a4786a1f17bce9a0b05d980c9f7b9e085a0b7b6
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2019-01-06 01:29:00 +0100

    Update CTU CAN FD core to version after decoupling.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- db4cgx15_pcie_ctu_can_fd/db4cgx15_pcie_ctu_can_fd.vhd
+++ db4cgx15_pcie_ctu_can_fd/db4cgx15_pcie_ctu_can_fd.vhd
@@ -43,8 +43,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.CANconstants.all;
-use work.CANcomponents.all;
+use work.can_constants.all;
+use work.can_components.all;
 
 library altera;
 use altera.altera_syn_attributes.all;
